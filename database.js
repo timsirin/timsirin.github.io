197e8c44a0b1c0706294535722fd719d9e9df065
Tahar Boukhenoufa <tahar.boukhenoufa@gmail.com>
2026-08-21 16:13:31 +0200

    Mise à jour de database.js pour Neon (SSL rejectUnauthorized: false)

--- database.js
+++ database.js
@@ -2,9 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Requis par Neon.tech et Render
-  }
+  ssl: { rejectUnauthorized: false }
 });
 
 const db = {
